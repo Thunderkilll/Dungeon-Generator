d7b5d8fffebb26cfce7f960b1695f2f3aa75fb6b
khaled guesmi <khaled.guesmi@esprit.tn>
2022-09-28 17:31:33 +0100

    update game logic on start

--- Assets/Scripts/Network/NetworkManagerUI.cs
+++ Assets/Scripts/Network/NetworkManagerUI.cs
@@ -10,21 +10,56 @@ public class NetworkManagerUI : MonoBehaviour
     [SerializeField] private Button serverBtn;
     [SerializeField] private Button hostBtn;
     [SerializeField] private Button clientBtn;
+    [SerializeField] private CameraFollow cameraFollow;
+    [SerializeField] private PlayerSurvival playerSurvivalScript;
+    [SerializeField] private AudioManager audioManager;
+    [SerializeField] private MusicManager musicManager;
+    [SerializeField] private UIController uIController;
+    [SerializeField] private GameStateManager gameStateManager;
 
 
     void Awake()
     {
         serverBtn.onClick.AddListener(() =>
         {
+
             NetworkManager.Singleton.StartServer();
+            ActivateScripts();
         });
         hostBtn.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartHost();
+            ActivateScripts();
         });
         clientBtn.onClick.AddListener(() =>
         {
             NetworkManager.Singleton.StartClient();
+            ActivateScripts();
         });
+    }
+
+    void DeactivateScripts()
+    {
+        cameraFollow.enabled = false;
+        audioManager.enabled = false;
+        musicManager.enabled = false;
+        playerSurvivalScript.enabled = false;
+        gameStateManager.enabled = false;
+        uIController.enabled = false;
+
+    }
+
+    void ActivateScripts()
+    {
+        cameraFollow.enabled = true;
+        audioManager.enabled = true;
+        musicManager.enabled = true;
+        playerSurvivalScript.enabled = true;
+        gameStateManager.enabled = true;
+        uIController.enabled = true;
+    }
+    private void Start()
+    {
+        DeactivateScripts();
     }
 }
